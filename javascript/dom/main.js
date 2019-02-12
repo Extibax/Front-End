192ce87f62270272b1f81d5905d98a58960d959e
Juan Bedoya <extibax@gmail.com>
2019-02-11 23:20:17 -0500

    DOM

---
+++ javascript/dom/main.js
@@ -0,0 +1,7 @@
+/* var boxes = document.getElementsByTagName('div'); */
+/* var boxes = document.getElementsByClassName('box'); */
+
+var boxes = document.getElementById('first-box');
+
+/* boxes[0].textContent = 'Hello World'; */
+/* boxes[0].innerHTML = '<h1>Hello!</h1>'; */

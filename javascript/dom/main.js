c32699200560afd58b51d5609fe0d5dc6e9e611c
Extibax <extibax@gmail.com>
2019-02-23 23:09:20 -0500

    DOM events, this

--- javascript/dom/main.js
+++ javascript/dom/main.js
@@ -1,7 +1,33 @@
-/* var boxes = document.getElementsByTagName('div'); */
+var boxes = document.getElementsByTagName('div');
 /* var boxes = document.getElementsByClassName('box'); */
+var firstBox = document.getElementById('first-box');
 
-var boxes = document.getElementById('first-box');
+/* firstBox.textContent = 'Hello World';
+firstBox.innerHTML = '<u>Hello World</u>'; */
 
-/* boxes[0].textContent = 'Hello World'; */
-/* boxes[0].innerHTML = '<h1>Hello!</h1>'; */
+//Creating nodes
+
+//1.Create the element
+var box = document.createElement('div');
+//2.Create one text node
+var content = document.createTextNode('Extibax Programmer');
+//3.Add the text node to the element
+box.appendChild(content);
+//4.Add attributes to the box
+box.setAttribute('class', 'box orange');
+
+//5.Add the element to the document
+var container = document.getElementById('container');
+container.appendChild(box);
+
+//Modifying the class or id of an element
+box.id = 'first';
+box.className = 'box orange';
+
+var father = boxes[0].parentNode;
+
+/* father.insertBefore(box, boxes[2]); */
+
+father.replaceChild(box, boxes[2]);
+
+father.removeChild(boxes[3]);
